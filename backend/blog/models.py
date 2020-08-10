55ba9bb3fdfdfd0499bd5e6c0053aa39c5aa8839
Chih-Wei, Hsu <kevin860612@gmail.com>
2020-08-10 18:30:23 +0800

    add blog model

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(unique=True, max_length=50, verbose_name='Title')
+    content = models.TextField(verbose_name='Content')
+    editTime = models.DateTimeField(auto_now=True, verbose_name='Edit Time')
