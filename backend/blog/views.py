2137085da2f6b234ca2db7917a77885cec12d522
Kevin <kevin860612@gmail.com>
2020-08-11 10:08:44 +0800

    listing view

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,5 +1,7 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from .models import Blog
 
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the blog index.")
+    blogs = Blog.objects.all()
+    return render(request, 'index.html', {'blogs': blogs})
