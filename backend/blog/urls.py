c56acc0302eae507ad5176d1283c969aaef81d80
Kevin <kevin860612@gmail.com>
2020-08-11 13:50:58 +0800

    restframework

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
-from .views import index
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import index, blogViewSet
+
+router = DefaultRouter()
+router.register('blog', blogViewSet)
 
 urlpatterns = [
     path('', index, name='Index'),
+    path('api/', include(router.urls)),
 ]
--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 from .models import Blog
+from .serializers import BlogSerializer
 
 
 def index(request):
     blogs = Blog.objects.all()
     return render(request, 'index.html', {'blogs': blogs})
+
+
+class blogViewSet(ModelViewSet):
+    serializer_class = BlogSerializer
+    queryset = Blog.objects.all()
