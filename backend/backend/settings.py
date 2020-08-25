4ffb2be2b73afbf46a1eeac099e679501c10af22
Kevin <kevin860612@gmail.com>
2020-08-25 16:28:11 +0800

    login logout

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -114,3 +114,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+LOGIN_URL = '/login/'
+
+LOGIN_REDIRECT_URL = '/blog/'
+
+LOGOUT_REDIRECT_URL = '/login/'
--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from django.contrib.auth.views import LoginView, LogoutView
 from .views import index, blogViewSet, BlogList, BlogCreate, BlogUpdate
 
 router = DefaultRouter()
@@ -7,6 +8,8 @@ router.register('blog', blogViewSet)
 
 urlpatterns = [
     path('', index, name='Index'),
+    path('login/', LoginView.as_view(template_name='form.html'), name='Login'),
+    path('logout/', LogoutView.as_view(), name='Logout'),
     path('blog/', BlogList.as_view(), name='BlogList'),
     path('blog/create/', BlogCreate.as_view(), name='BlogCreate'),
     path('blog/update/<pk>/', BlogUpdate.as_view(), name='BlogUpdate'),
