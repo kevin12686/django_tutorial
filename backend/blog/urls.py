56086570494c261082631fdabe922137a10d51cf
Kevin <kevin860612@gmail.com>
2020-08-25 14:40:57 +0800

    ListView

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import index, blogViewSet
+from .views import index, blogViewSet, BlogList
 
 router = DefaultRouter()
 router.register('blog', blogViewSet)
 
 urlpatterns = [
     path('', index, name='Index'),
+    path('blog/', BlogList.as_view(), name='BlogList'),
     path('api/', include(router.urls)),
 ]
--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 from rest_framework.viewsets import ModelViewSet
 from .models import Blog
 from .serializers import BlogSerializer
@@ -12,3 +13,8 @@ def index(request):
 class blogViewSet(ModelViewSet):
     serializer_class = BlogSerializer
     queryset = Blog.objects.all()
+
+
+class BlogList(ListView):
+    model = Blog
+    template_name = 'blogList.html'
