df6d29c4cf7b9df5bac59ca6a4ac50afedfc44bf
Kevin <kevin860612@gmail.com>
2020-08-25 15:31:35 +0800

    CreateView

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import index, blogViewSet, BlogList
+from .views import index, blogViewSet, BlogList, BlogCreate
 
 router = DefaultRouter()
 router.register('blog', blogViewSet)
@@ -8,5 +8,6 @@ router.register('blog', blogViewSet)
 urlpatterns = [
     path('', index, name='Index'),
     path('blog/', BlogList.as_view(), name='BlogList'),
+    path('blog/create/', BlogCreate.as_view(), name='BlogCreate'),
     path('api/', include(router.urls)),
 ]
--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.urls import reverse
+from django.views.generic import ListView, CreateView
 from rest_framework.viewsets import ModelViewSet
 from .models import Blog
 from .serializers import BlogSerializer
@@ -18,3 +19,12 @@ class blogViewSet(ModelViewSet):
 class BlogList(ListView):
     model = Blog
     template_name = 'blogList.html'
+
+
+class BlogCreate(CreateView):
+    model = Blog
+    fields = '__all__'
+    template_name = 'form.html'
+
+    def get_success_url(self):
+        return reverse('BlogList')
